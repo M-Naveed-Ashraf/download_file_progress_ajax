9b6ce25b47b71ced3d064bf47f1e1577c5bf3303
m-naveed-ashraf <mahernaveed531@gmail.com>
2022-11-11 14:17:42 +0500

    js file initialized

---
+++ index.js
@@ -0,0 +1,52 @@
+var fileName = "a.mkv"
+var progress = document.getElementById("progress");
+var progressText = document.getElementById("progress-text");
+
+var downloadProgressText = document.getElementById("download-progress-text");
+
+function download() {
+    var startTime = new Date().getTime();
+    //// previous code in download button click listener
+    request = new XMLHttpRequest();
+    request.responseType = 'blob';
+    request.open('get', fileName, true);
+    request.send();
+    request.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var obj = window.URL.createObjectURL(this.response);
+            document.getElementById('save-file').setAttribute('href', obj);
+
+            document.getElementById('save-file').setAttribute('download', fileName);
+            setTimeout(function() {
+                window.URL.revokeObjectURL(obj);
+            }, 60 * 1000);
+        }
+    };
+    request.onprogress = function(e) {
+        progress.ariaValueMax = e.total;
+        progress.value = e.loaded;
+
+        var percent_complete = (e.loaded / e.total) * 100;
+        percent_complete = Math.floor(percent_complete);
+
+        var duration = ( new Date().getTime() - startTime ) / 1000;
+        var bps = e.loaded / duration;
+        var kbps = bps / 1024;
+        kbps = Math.floor(kbps);
+
+        var time = (e.total - e.loaded) / bps;
+        var seconds = time % 60;
+        var minutes = time / 60;
+
+        seconds = Math.floor(seconds);
+        minutes = Math.floor(minutes);
+
+        progress.setAttribute("aria-valuemax", e.total);
+        progress.setAttribute("aria-valuenow", e.loaded);
+        progress.style.width = percent_complete + "%";
+        progress.innerHTML = percent_complete + "%";
+
+        downloadProgressText.innerHTML = kbps + " KB / s" + "<br>" + minutes + " min " + seconds + " sec remaining";
+
+    };
+};
